68b84e4fa2d8aec675e8a593162905879e3e5249
jpm172 <moynahanjaredp@gmail.com>
2024-04-05 18:36:26 -0400

    chagned setUp

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -36,10 +36,15 @@ public class D3Test {
   private Map<String, Object> vars;
   JavascriptExecutor js;
   @Before
-  public void setUp() {
-    driver = new ChromeDriver();
+  public void setUp() 
+  {
+
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
+
   }
   @After
   public void tearDown() {
